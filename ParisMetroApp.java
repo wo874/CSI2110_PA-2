4279b3f614e02588b4b4dd5470c694367212637a
wo874 <92176181+wo874@users.noreply.github.com>
2023-11-27 16:34:24 -0500

    Update ParisMetroApp.java

--- ParisMetroApp.java
+++ ParisMetroApp.java
@@ -7,23 +7,39 @@ public class ParisMetroApp {
             ParisMetro parisMetro = new ParisMetro(metroGraph);
 
             if (args.length == 1) {
-                // Question 2-i: Identify all stations belonging to the same line of a given station
                 int station = Integer.parseInt(args[0]);
+                System.out.println("Test -----------------------------------");
+                System.out.println("    Input:");
+                System.out.println("    N1 = " + station);
+                System.out.println("    Output:");
                 parisMetro.findStationsInSameLine(station);
+                System.out.println("End of Test -----------------------------");
             } else if (args.length == 2) {
-                // Question 2-ii: Find the shortest path between two stations
                 int source = Integer.parseInt(args[0]);
                 int destination = Integer.parseInt(args[1]);
+                System.out.println("Test -----------------------------------");
+                System.out.println("    Input:");
+                System.out.println("    N1 = " + source);
+                System.out.println("    Output:");
                 parisMetro.findShortestPath(source, destination);
+                System.out.println("End of Test -----------------------------");
             } else if (args.length == 3) {
-                // Question 2-iii: Find the shortest path with a broken line
                 int source = Integer.parseInt(args[0]);
                 int destination = Integer.parseInt(args[1]);
                 int brokenLineEndpoint = Integer.parseInt(args[2]);
+                System.out.println("Test -----------------------------------");
+                System.out.println("    Input:");
+                System.out.println("    N1 = " + source);
+                System.out.println("    Output:");
                 parisMetro.findShortestPathWithBrokenLine(source, destination, brokenLineEndpoint);
+                System.out.println("End of Test -----------------------------");
             } else {
-                // Bonus Question iv: Determine the minimum number of broken lines
+                System.out.println("Test -----------------------------------");
+                System.out.println("    Input:");
+                System.out.println("    No specific input for bonus question");
+                System.out.println("    Output:");
                 parisMetro.findMinBrokenLines();
+                System.out.println("End of Test -----------------------------");
             }
         } catch (IOException e) {
             e.printStackTrace();
