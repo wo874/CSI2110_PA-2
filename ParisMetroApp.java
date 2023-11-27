a3dd8fb1247999e9033a7376968b331803b29e28
wo874 <92176181+wo874@users.noreply.github.com>
2023-11-27 17:38:29 -0500

    Update ParisMetroApp.java

--- ParisMetroApp.java
+++ ParisMetroApp.java
@@ -1,47 +1,57 @@
 import java.io.IOException;
 
 public class ParisMetroApp {
+    
     public static void main(String[] args) {
-        try {
-            Graph metroGraph = MetroReader.readMetro("metro.txt");
-            ParisMetro parisMetro = new ParisMetro(metroGraph);
 
-            if (args.length == 1) {
+        try {
+            Graph metroGraph=MetroReader.readMetro("metro.txt");
+
+            ParisMetro parisMetro=new ParisMetro(metroGraph);
+
+            if (args.length==1) {
                 int station = Integer.parseInt(args[0]);
-                System.out.println("Test -----------------------------------");
+                System.out.println("Test ★・・・・・・★★・・・・・・★★・・・・・・★");
                 System.out.println("    Input:");
                 System.out.println("    N1 = " + station);
                 System.out.println("    Output:");
                 parisMetro.findStationsInSameLine(station);
-                System.out.println("End of Test -----------------------------");
-            } else if (args.length == 2) {
+                System.out.println("End of Test ★・・・・・・★★・・・・・・★★・・・・・・★");
+
+
+            } else if (args.length==2) {
                 int source = Integer.parseInt(args[0]);
                 int destination = Integer.parseInt(args[1]);
-                System.out.println("Test -----------------------------------");
+                System.out.println("Test ★・・・・・・★★・・・・・・★★・・・・・・★");
                 System.out.println("    Input:");
                 System.out.println("    N1 = " + source);
                 System.out.println("    Output:");
                 parisMetro.findShortestPath(source, destination);
-                System.out.println("End of Test -----------------------------");
-            } else if (args.length == 3) {
+                System.out.println("End of Test ★・・・・・・★★・・・・・・★★・・・・・・★");
+
+
+            } else if (args.length==3) {
                 int source = Integer.parseInt(args[0]);
                 int destination = Integer.parseInt(args[1]);
                 int brokenLineEndpoint = Integer.parseInt(args[2]);
-                System.out.println("Test -----------------------------------");
+                System.out.println("Test ★・・・・・・★★・・・・・・★★・・・・・・★");
                 System.out.println("    Input:");
                 System.out.println("    N1 = " + source);
                 System.out.println("    Output:");
                 parisMetro.findShortestPathWithBrokenLine(source, destination, brokenLineEndpoint);
-                System.out.println("End of Test -----------------------------");
+                System.out.println("End of Test ★・・・・・・★★・・・・・・★★・・・・・・★");
+
+
             } else {
-                System.out.println("Test -----------------------------------");
+                System.out.println("Test ★・・・・・・★★・・・・・・★★・・・・・・★");
                 System.out.println("    Input:");
                 System.out.println("    No specific input for bonus question");
                 System.out.println("    Output:");
                 parisMetro.findMinBrokenLines();
-                System.out.println("End of Test -----------------------------");
+                System.out.println("End of Test ★・・・・・・★★・・・・・・★★・・・・・・★");
             }
         } catch (IOException e) {
+
             e.printStackTrace();
         }
     }
