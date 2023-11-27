9d0be98ea1849c61352d258b628100c2ec8f1c27
wo874 <92176181+wo874@users.noreply.github.com>
2023-11-27 17:44:34 -0500

    Update MetroReader.java

--- MetroReader.java
+++ MetroReader.java
@@ -1,30 +1,36 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+
 import java.io.IOException;
 
 class MetroReader {
-    // Reads the metro information from the given file and constructs the graph
+   
     public static Graph readMetro(String fileName) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(fileName));
-        Graph metroGraph = new Graph();
+        BufferedReader reader=new BufferedReader(new FileReader(metro.txt));
 
-        // Reading the number of vertices and edges
+        Graph metroGraph=new Graph();
+
+        // scaning # of vertices & edges
         int numVertices = Integer.parseInt(reader.readLine().trim());
         int numEdges = Integer.parseInt(reader.readLine().trim());
 
-        // Reading and adding vertices to the graph
+        // scaning & adding vertices to graph
         for (int i = 0; i < numVertices; i++) {
-            String[] vertexInfo = reader.readLine().trim().split(" ");
-            int vertexNumber = Integer.parseInt(vertexInfo[0]);
+
+            String[] vertexInfo=reader.readLine().trim().split(" ");
+            int vertexNumber=Integer.parseInt(vertexInfo[0]);
+
             metroGraph.addVertex(vertexNumber);
         }
 
-        // Reading and adding edges to the graph
-        for (int i = 0; i < numEdges; i++) {
-            String[] edgeInfo = reader.readLine().trim().split(" ");
-            int source = Integer.parseInt(edgeInfo[0]);
-            int destination = Integer.parseInt(edgeInfo[1]);
-            int weight = Integer.parseInt(edgeInfo[2]);
+        // scaning & adding edges to graph
+        for (int i=0; i < numEdges; i++) {
+
+            String[] edgeInfo=reader.readLine().trim().split(" ");
+            int source=Integer.parseInt(edgeInfo[0]);
+            int destination=Integer.parseInt(edgeInfo[1]);
+            int weight=Integer.parseInt(edgeInfo[2]);
+
             metroGraph.addEdge(source, destination, weight);
         }
 
